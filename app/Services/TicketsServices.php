977e809155906a77c8a32eaf8591247143e9a314
Tomas Crespo <enritecnolol@gmail.com>
2020-11-02 09:09:09 -0400

    gets tickets

--- app/Services/TicketsServices.php
+++ app/Services/TicketsServices.php
@@ -205,8 +205,8 @@ class TicketsServices
     {
         $tickets = DB::connection('client')
             ->table('public.tickets')
-//            ->where('title', $search)
-//            ->where('status', true)
+            ->where('title', $search)
+            ->where('status', true)
             ->paginate($size);
 
         return $tickets;
